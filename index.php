050582badbc9a3e10d5da983b39beec79c5b16fc
aMadAlien <irasibil2@gmail.com>
2022-07-14 11:44:39 +0300

    install a package

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 
 require 'vendor/autoload.php';
 use iren\composer_lesson\Library;
-use iren\composer_lesson\App;
+use psr\log;
 
 echo("<h1>Wellcome to the easy calculator written by php</h1> <hr>");
 
 $library = new Library();
 $library->library();
 
-$app = new App;
-$app->App();
+$log = new Log;
